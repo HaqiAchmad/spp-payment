c322f86a0de3ef230bd8341c9edf3ff0a6e3e9cf
Achmad Baihaqi <hakiahmad756@gmail.com>
2021-03-09 19:44:03 +0700

    update Main.java

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,18 +1,19 @@
 package com.main;
 
 /**
- *
+ * Class ini adala class yang pertama kali dieksekusi saat program dijalankan
+ * 
  * @author Achmad Baihaqi
- * @since 2021-03-01
+ * @since 2021-03-07
  */
 public class Main {
-    
+
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable(){
             
             @Override
             public void run(){
-                new com.window.LoginWindow().setVisible(true);
+                new com.window.SplashWindow().setVisible(true);
             }
         });
     }
