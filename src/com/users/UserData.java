230761669b8b8b439df92d8dab04a30083b0c8a1
Achmad Baihaqi <hakiahmad756@gmail.com>
2021-06-19 19:12:45 +0700

    menambahkan class UserData.java

---
+++ src/com/users/UserData.java
@@ -0,0 +1,18 @@
+package com.users;
+
+/**
+ *
+ * @author Achmad Baihaqi
+ * @since 2021-06-14
+ */
+public enum UserData {
+    
+    ID_USER,
+    NO_HP,
+    EMAIL,
+    FOTO_PROFILE,
+    PASSWORD,
+    LEVEL,
+    ID_LOGIN,
+    LAST_ON
+}
