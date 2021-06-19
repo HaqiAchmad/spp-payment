931d64f1dab4fb135e77499fce69e8996259d5be
Achmad Baihaqi <hakiahmad756@gmail.com>
2021-06-19 17:54:05 +0700

    menambahkan class SiswaData.java

---
+++ src/com/users/SiswaData.java
@@ -0,0 +1,19 @@
+package com.users;
+
+/**
+ *
+ * @author Achmad Baihaqi
+ * @since 2021-06-14
+ */
+public enum SiswaData {
+    
+    NIS,
+    NAMA_SISWA,
+    GENDER,
+    TEMPAT_LHR,
+    TANGGAL_LHR,
+    ALAMAT,
+    ID_KELAS,
+    NAMA_WALI,
+    ID_SPP
+}
