904f8e8ad6b7d3604a7c8a519f933ec89a1fe0b0
Achmad Baihaqi <hakiahmad756@gmail.com>
2021-06-19 17:13:30 +0700

    menambahkan class PetugasData.java

---
+++ src/com/users/PetugasData.java
@@ -0,0 +1,16 @@
+package com.users;
+
+/**
+ *
+ * @author Achmad Baihaqi
+ * @since 2021-06-14
+ */
+public enum PetugasData {
+    
+    ID_PETUGAS,
+    NAMA_PETUGAS,
+    GENDER,
+    TEMPAT_LHR,
+    TANGGAL_LHR,
+    ALAMAT
+}
