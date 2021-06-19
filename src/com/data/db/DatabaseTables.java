766307a75469ab399f22b4b16753ec1bfd8a488d
Achmad Baihaqi <hakiahmad756@gmail.com>
2021-06-19 13:16:45 +0700

    add DatabaseTables.java

---
+++ src/com/data/db/DatabaseTables.java
@@ -0,0 +1,18 @@
+package com.data.db;
+
+/**
+ *
+ * @author Achmad Baihaqi
+ * @since 2021-06-14
+ */
+public enum DatabaseTables {
+    
+    USERS,
+    SISWA,
+    PETUGAS,
+    LOGIN,
+    KELAS,
+    SPP,
+    PEMBAYARAN
+    
+}
